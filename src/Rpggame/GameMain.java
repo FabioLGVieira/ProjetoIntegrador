46462825ae6c341f767580c74be68490eb1d51e1
fabio <vieira.fa@hotmail.com>
2019-04-10 18:15:56 -0300

    implementação de perguntas

--- src/Rpggame/GameMain.java
+++ src/Rpggame/GameMain.java
@@ -1,9 +1,13 @@
 package Rpggame;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class GameMain {
 
+    static Random randomico = new Random();
     static Scanner sc = new Scanner(System.in);
     static boolean running = false;
     static String pegaComando;
@@ -23,8 +27,7 @@ public class GameMain {
         System.out.println("Comandos disponiveis: Leste | Oeste");
 
         //tutorial pro player aprender alguns comandos
-        
-        
+        strNext();
         while (!pegaComando.equals("oeste")) {
             System.out.println("A biblioteca é para o outro lado!");
             strNext();
@@ -54,14 +57,55 @@ public class GameMain {
         }
         System.out.println("O que é esse papel amassado no chao?");
         System.out.println("Comandos disponiveis: Pegar | Ignorar");
-        if(pegaComando.equals("pegar")){
+        if (pegaComando.equals("pegar")) {
             System.out.println("Wow é um projeto detalhado de um Foguete Espacial, quem deixaria isso aqui?");
         } else {
             System.out.println("Vou pegar esse livro de Java e reprovar na prova mesmo!");
         }
     }
-    
-    public static void strNext(){
+
+    public static void perguntaFacil() {
+        int tentativas = 3;
+        int rnd = randomico.nextInt(2);
+        String perguntas[] = {"O conjunto {x (pertence) R / 2,5 < x <= 15} pode ser representado pelo seguinte intervalo",
+            "O número 255 na base decimal, pode ser representado, respectivamente, nas bases Binária, Octal e Hexadecimal por",
+            "Quantos bits há em 2 bytes", "A expressão (a-b)³ pode ser representada por"},
+                respostasFaceis[][] = {{"[ 10/4 ; 15 ]", "[ 2,5 ; 15 [", "] 10/4 ; 15 ]", "] 2,5 ; 15 ["},
+                {"01100110, 356 ,EE", "00001111, 380, EF", "11111111, 377, FF", "11110000, 374, FE"},
+                {"8", "16", "32", "64"}, {"a³ - 3a³b + 3ab³ + b³", "a³ + 3a³b + 3ab³ + b³", "a³ - 3a³b - 3ab³ - b³", "a³ - 3a³b - 3ab³ + b³"}
+                },
+                alternativaA, alternativaB, alternativaC, alternativaD;
+        char resposta, corretaFaceis[] = {'c', 'c', 'b', 'a'};
+        alternativaA = respostasFaceis[rnd][0];
+        alternativaB = respostasFaceis[rnd][1];
+        alternativaC = respostasFaceis[rnd][2];
+        alternativaD = respostasFaceis[rnd][3];
+
+        System.out.println(perguntas[rnd] + ":");
+        System.out.println("a)" + alternativaA);
+        System.out.println("b)" + alternativaB);
+        System.out.println("c)" + alternativaC);
+        System.out.println("d)" + alternativaD);
+        do {
+            System.out.println("Voce tem " + tentativas + "tentativas");
+            resposta = sc.next().toLowerCase().charAt(0);
+            if (resposta != corretaFaceis[rnd]) {
+                System.out.println("Voce errou tente denovo");
+                tentativas--;
+            }
+        } while (resposta != corretaFaceis[rnd] && tentativas > 0);
+        if (resposta == corretaFaceis[rnd]) {
+            System.out.println("resposta certa");
+        } else {
+            System.out.println("Gostaria de tentar outra pergunta? S/ N");
+        }
+        resposta = sc.next().toLowerCase().charAt(0);
+        if (resposta == 's') {
+            perguntaFacil();
+        }
+    }
+
+    public static void strNext() {
         pegaComando = sc.nextLine().toLowerCase();
     }
 
@@ -95,6 +139,7 @@ public class GameMain {
                 game();
                 break;
             case 3:
+                perguntaFacil(); // apenas acelerar o teste, tirar futuramente
                 System.out.println("Pegadinha haha");
                 break;
             case 4:
