ab6c033044a5fe04cbb6accc743d9ea81ba4826e
pedrohfarantes <phfarantes96@gmail.com>
2019-05-28 18:27:12 -0300

    continuacao inicio

--- src/Rpggame/Sonho.java
+++ src/Rpggame/Sonho.java
@@ -56,39 +56,95 @@ public class Sonho {
     static void sair() {
         System.exit(0); 
     }
-    static String verificaNome(String nome){
-        Scanner sc = new Scanner(System.in);
-        do {
-            System.out.println("[Digite seu nome...]");
-            nome = sc.nextLine();
-        } while (nome.isEmpty() || nome.equals("") || nome.charAt(0) == ' ');
-        return nome;
+          
+    static void imprimirTexto(String vet []){
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet [i]);
+        }
     }
     
-    static void sonho(String [][] v){
+    
+    static void sonho(){
         System.out.println(" ========================================================================= ");
-        System.out.println(" Voce é um aluno de Tecnologia em analise e desnvolvimento de sistemas ");
-        System.out.println(" do Senac e está muito preocupado com a faculdade, não para de pensar ");
-        System.out.println(" nela nem quando está dormino [...] ");
+        String d1[] = {" Voce é um aluno de Tecnologia em analise e desnvolvimento de sistemas \n",
+        " do Senac e está muito preocupado com a faculdade, não para de pensar \n", 
+        " nela nem quando está dormino [...] \n"};
+        imprimirTexto(d1);
         System.out.println(" ========================================================================= ");
-        System.out.println(" [...]Dutante o sonho ");
-        System.out.println(" Voce esta chegando ao centro universitário senac para mais uma semana de aulas ");
-        System.out.println(" e se depara com uma situação muito estranha [...] ");
-        System.out.println(" Um et, isso mesmo, um et vem falar com voce[...] ");
+        String d2 [] = {" [...]Dutante o sonho \n ",
+        " Voce esta chegando ao centro universitário senac para mais uma semana de aulas \n",
+        " e se depara com uma situação muito estranha [...] \n",
+        " Um et, isso mesmo, um et vem falar com voce[...] \n"};
+        imprimirTexto(d2);        
         System.out.println(" ========================================================================= ");
-        System.out.println(" Et: olá terráqueo, meu nome é Billy, venho de muito longe e voce foi o escolhido ");
-        System.out.println(" Primeiramente me confirme seu nome, para ter certeza que é voce mesmo ");
-        v[0][0] = verificaNome(v[0][0]);
-        System.out.println("Voce mesmo! " + v[0][0]);
-        System.out.println(v[0][0] + " : pra que fui escolhido ");
-        System.out.println(" Billy: Para uma missão[...] ");
-        System.out.println(" Nos ajudar a construir um foguete movido a base de nosso alimento preferido aqui na Terra ");
-        System.out.println(" O foguete tem que ser movido a Cheadar, voce devera nos ajudar porque seu destino depende disso ");
-        System.out.println(" Seus professores estão cientes de nossa invasão e eles o ajudaram a coletar peças para o nosso foguete " );
-        System.out.println(" Fale com eles para consgui - las ");
+        String d3 [] = {" Et: olá terráqueo, meu nome é Billy, venho de muito longe e voce foi o escolhido \n",
+        " Primeiramente me confirme seu nome, para ter certeza que é voce mesmo o escolhido \n"};        
+        imprimirTexto(d3);
         System.out.println(" ========================================================================= ");
+        String d4 [] = {"Voce mesmo! \n", " : pra que fui escolhido ", " Billy: Para uma missão[...] ", 
+        " Nos ajudar a construir um foguete movido a base de nosso alimento preferido aqui na Terra ",
+        " O foguete tem que ser movido a Cheadar, voce devera nos ajudar porque seu destino depende disso ",
+        " Fale com eles para consgui - las "};      
+        imprimirTexto(d4);        
+        System.out.println(" ========================================================================= ");
+        String d5 [] = {" Para conseguir sua primeira peça siga até a Nasa e fale com um de meus amigos "};
+        imprimirTexto(d5);
     }
-
+    
+    //primeira peça: tanque de cheddar
+    static void Nasa(){
+    //falar com amigo do et da entrada    
+    }
+    static void salaAdm(){
+    //pergunta adm
+    //pegar tanque de cheddar
+    }
+    
+    //segunda peça:fuselagem
+    static void Nasa2(){
+    //falar de novo com o amigo do et    
+    }
+    static void estacionamento(){
+        
+    }
+    static void salaPreCalculo(){
+    //pergunta pré calculo
+    //pegar fuselagem
+    }
+    //terceira peça: turbina
+    static void Nasa3(){
+    //falar de novo com o amigo do et    
+    }
+    static void cubao(){
+        
+    }
+    static void salaConceitos(){
+    //pergunta conceitos
+    //pegar turbina
+    }
+    //quarta peça: computador de bordo 
+    static  void Nasa4(){
+    //falar de novo com o amigo do et    
+    }
+    static void cit(){
+    //centro de inovação tecnologica    
+    }
+    static void salaAlgoritimos(){
+    //pergunta algoritimos
+    //pegar computador de bordo
+    }
+    //quinta peça: comida
+    static void Nasa5(){
+    //falar de novo com o amigo do et    
+    }
+    static void p1(){
+    //pegar comida    
+    }
+    static void labMecanica(){
+    //montar o foguete
+   //entregar o foguete    
+    }      
+          
     public static void main(String[] args) {
         lerMenu();
 
